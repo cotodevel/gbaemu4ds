09d94919fc8d95bfaa9e0ad2b5ab2035f07829fb
programmer.coto.1988@gmail.com <programmer.coto.1988@gmail.com>
2017-11-06 10:03:23 -0400

    fix silly rtc copy-paste bug

--- TGDS/arm9/source/RTC.cpp
+++ TGDS/arm9/source/RTC.cpp
@@ -198,9 +198,9 @@ bool rtcWrite(u32 address, u16 value)
                 */
                 
 				rtcClockData.dataLen = 3;
-                rtcClockData.data[4] = toBCD(getTime()->tm_hour);
-                rtcClockData.data[5] = toBCD(getTime()->tm_min);
-                rtcClockData.data[6] = toBCD(getTime()->tm_sec);
+                rtcClockData.data[0] = toBCD(getTime()->tm_hour);
+                rtcClockData.data[1] = toBCD(getTime()->tm_min);
+                rtcClockData.data[2] = toBCD(getTime()->tm_sec);
 				rtcClockData.state = DATA;
                 
               }
